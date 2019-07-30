a846973a50c46616d7e5460dcf044106a77771a1
地主兄 <yangman945@163.com>
2019-07-31 02:25:59 +0800

    商品分类页面数据的渲染ok

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,46 @@
-// pages/category/index.js
+import {promise} from "../../request/promise.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    current:0,
+    // 商品分类左侧菜单
+    left_menuList:[],
+    // 商品分类右侧菜单
+    right_menuList:[],
+    // 右侧滚动条激活时的距离
+    scrolltop:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 新增一个全局变量，用来接收请求的数据
+  ovrelist : [],
+  onLoad(){
+    this.getCategory()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取商品分类数据
+  getCategory(){
+    promise({url:'/categories'}).then(result=>{
+      // console.log(result)
+      this.ovrelist = result.data.message
+      // 获取数据后我们分别将左侧右侧需要渲染的数据剥离开来
+      const left_menuList = this.ovrelist.map(v=>(
+        // 左侧
+        {cat_id:v.cat_id,cat_name:v.cat_name} 
+      ))
+        // 右侧
+      const right_menuList = this.ovrelist[0].children
+      // this.list = result.data.message[0].children
+        this.setData({
+          left_menuList,
+          right_menuList
+        })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleChange(e){
+    const { index } = e.target.dataset
+    const right_menuList = this.ovrelist[index].children
+    this.setData({
+      current:index,
+      right_menuList,
+      // 小优化，点击左侧菜单栏是右侧滚动回顶部
+      scrolltop:0
+     })
   }
 })
