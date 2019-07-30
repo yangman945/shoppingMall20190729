3d2bbc572d8d72444e2465406282b53986c9d318
地主兄 <yangman945@163.com>
2019-07-30 13:57:42 +0800

    轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,23 @@
 // pages/index/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 轮播图
+    swiperList:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 页面加载时触发
+  onLoad(){
+    this.getSwaper()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getSwaper(){
+   wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+        // console.log(result)
+        this.setData({
+          swiperList:result.data.message
+        })
+      },
+    });
+      
   }
 })
