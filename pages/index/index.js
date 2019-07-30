28de879fad8e973c9d35ce4fd9d58d0718e09a26
地主兄 <yangman945@163.com>
2019-07-30 17:23:26 +0800

    商品楼层完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,12 +2,19 @@
 Page({
   data: {
     // 轮播图
-    swiperList:[]
+    swiperList:[],
+    // 导航栏
+    navList:[],
+    // 商品楼层数据
+    floorList:[]
   },
   // 页面加载时触发
   onLoad(){
     this.getSwaper()
+    this.getNav()
+    this.getGoodsFloor()
   },
+  // 获取轮播图
   getSwaper(){
    wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
@@ -17,7 +24,32 @@ Page({
           swiperList:result.data.message
         })
       },
+    });    
+  },
+  // 获取导航栏
+  getNav(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+        // console.log(result)
+        this.setData({
+          navList:result.data.message
+        })
+      },
+    });  
+  },
+  // 获取商品楼层
+  getGoodsFloor(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success: (result) => {
+        console.log(result)
+        this.setData({
+          floorList:result.data.message
+        })
+      },
     });
-      
   }
+  
+    
 })
