3129726d71b7a9ca82468b2b2fbc9f31a572a418
地主兄 <yangman945@163.com>
2019-08-02 17:40:54 +0800

    商品详情页静态页面的构建

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,27 @@
-// pages/goods_detail/index.js
+import {promise} from "../../request/promise.js"
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsDetailList:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(e){
+    console.log(e.goods_id)
+    this.getCateDetail(e.goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取商品详情内容
+ async getCateDetail(goods_id){
+    const { data } = await promise({url:'/goods/detail',data:{goods_id}})
+    // console.log(data.message)
+    // 返回的数据太多 我们只提取我们需要使用的
+    const goodsDetailList = {
+      goods_name:data.message.goods_name,
+      goods_price:data.message.goods_price,
+      goods_introduce:data.message.goods_introduce,
+      pics:data.message.pics
+    }
+    this.setData({
+      goodsDetailList
+    })
   }
 })
