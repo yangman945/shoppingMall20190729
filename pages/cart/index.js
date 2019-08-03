2fb9f0da818755975bcca1a33f30b658d5c2a65e
地主兄 <yangman945@163.com>
2019-08-04 00:46:28 +0800

    收货地址逻辑优化

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,31 +1,30 @@
-// pages/cart/index.js
+import { getSetting, openSetting, chooseAddress } from "../../utils/wx-async";
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
-  data: {},
-  handlechooseAddress() {
-    wx.getSetting({
-      // 当用户 没有点击授权信息undefined 或是 点击同意后true，调取收货地址的api
-      // 当用户点击取消后 下次再点击 我们需要弹出设置界面 让用户同意授权
-      success: result1 => {
-        const msg = result1.authSetting["scope.address"];
+  data: {
+    address: {}
+  },
 
-        if (msg === true || msg === undefined) {
-          wx.chooseAddress({
-            success: result2 => {
-              console.log(result2);
-            }
-          });
-        }else{
-          wx.openSetting({
-            success: (result3) => {
-              wx.chooseAddress({
-                success: result4 => {
-                  console.log(result4);
-                }
-              })
-            },
-          });  
-        }
-      }
+  async handlechooseAddress() {
+   try{
+    const res1 = await getSetting()
+    const msg = res1.authSetting["scope.address"];
+    console.log(msg)
+    
+    if(msg === false){
+      await openSetting()
+    }
+    const res2 = await chooseAddress()
+    console.log(res2),
+      // 将需要显示的地址信息拼接出来
+    res2.all = res2.provinceName + res2.cityName + res2.countyName + res2.detailInfo
+    wx.setStorageSync("address", res2);
+   } catch(err){}
+  },
+  // 在页面打开的时候将地址展示
+  onShow() {
+    this.setData({
+      address: wx.getStorageSync("address") || {}
     });
   }
 });
---
+++ utils/wx-async.js
@@ -0,0 +1,52 @@
+/**
+ * 以Promise形式改造代码
+ */
+
+ /**
+  * 获取用户当前设置
+  */
+ export const getSetting = () => {
+    return new Promise((resolve,reject)=>{
+        wx.getSetting({
+            success: (result) => {
+                resolve(result)
+            },
+            fail: (err) => {
+                reject(err)
+                console.log(err)
+            },
+        });     
+    })
+ }
+
+  /**
+  * 修改用户当前设置
+  */
+ export const openSetting = () => {
+    return new Promise((resolve,reject)=>{
+        wx.openSetting({
+            success: (result) => {
+                resolve(result)
+            },
+            fail: (err) => {
+                reject(err)
+            },
+        });     
+    })
+ }
+
+  /**
+  * 获取用户收货地址
+  */
+ export const chooseAddress = () => {
+    return new Promise((resolve,reject)=>{
+        wx.chooseAddress({
+            success: (result) => {
+                resolve(result)
+            },
+            fail: (err) => {
+                reject(err)
+            },
+        });     
+    })
+ }
