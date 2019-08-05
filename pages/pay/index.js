aa6ebcd7df7dde1ec4be0444e9580e0fa6ddae4b
地主兄 <yangman945@163.com>
2019-08-05 12:12:03 +0800

    商品结算页面结算逻辑判断完成

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,13 +1,15 @@
-// pages/pay/index.js
+import { promise } from "../../request/promise.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
+import { reqPay,Message } from "../../utils/wx-async.js";
 Page({
-  data:{
+  data: {
     // 地址信息
-    address:{},
-    totalPrice:0,
-    totalNum:0,
-    cart:{}
+    address: {},
+    totalPrice: 0,
+    totalNum: 0,
+    cart: {}
   },
-  onLoad(){
+  onLoad() {
     // 本地获取渲染于页面的信息
     // 获取地址
     let address = wx.getStorageSync("address") || {};
@@ -15,43 +17,94 @@ Page({
     let cart = wx.getStorageSync("cart") || {};
     // 遍历对象 将checked为false的属性删除
     for (const key in cart) {
-        if(!(cart[key].checked)){
-          delete cart[key]
-        }  
+      if (!cart[key].checked) {
+        delete cart[key];
+      }
     }
-    let cartArr = Object.values(cart)
+    let cartArr = Object.values(cart);
     // 总价格
-    let totalPrice = 0
+    let totalPrice = 0;
     // 总数量
-    let totalNum = 0
+    let totalNum = 0;
     // 将没有勾选的商品从购物车中过滤掉
-    cartArr.filter(v=>{
-       if(v.checked){
+    cartArr.filter(v => {
+      if (v.checked) {
         totalNum += v.num;
         totalPrice += v.goods_price * v.num;
       }
-    })
+    });
     this.setData({
       address,
       totalPrice,
       totalNum,
       cart
-    })
+    });
   },
-  handleOrderPay(){
-    // 对token进行获取判断 如果没有token进入授权页面获取
-    let token = wx.getStorageSync("token");
-    // console.log(token)
-    if(!token){
-      // 没有token去授权页面获取
-      wx.navigateTo({
-        url: '/pages/auth/index',
-      });
-        
-    }else{
-      // 有的话执行下一步
-      console.log('执行订单')
+  async handleOrderPay() {
+    try {
+      // 对token进行获取判断 如果没有token进入授权页面获取
+      let token = wx.getStorageSync("token");
+      let { cart } = this.data;
+      // console.log(token)
+      if (!token) {
+        // 没有token去授权页面获取
+        wx.navigateTo({
+          url: "/pages/auth/index"
+        });
+      } else {
+        // 有的话执行下一步
+        // 1准备获取创建订单需要的参数
+        let header = { Authorization: token };
+        let order_price = this.data.totalPrice;
+        let consignee_addr = this.data.address.all;
+        let goods = [];
+        for (const key in cart) {
+          goods.push({
+            goods_id: cart[key].goods_id,
+            goods_number: cart[key].num,
+            goods_price: cart[key].goods_price
+          });
+        }
+        let paramsList = { order_price, consignee_addr, goods };
+        // 2根据参数创建订单
+        const res = await promise({
+          url: "/my/orders/create",
+          header: header,
+          data: paramsList,
+          method: "POST"
+        });
+        // console.log(res)
+        const { order_number } = res.data.message;
+        // 3获取支付参数的pay
+        const res1 = await promise({
+          url: "/my/orders/req_unifiedorder",
+          header: header,
+          data: { order_number },
+          method: "POST"
+        });
+        // console.log(res1);
+        const { pay } = res1.data.message;
+        // console.log(pay);
+        // 4调用微信支付 微信内置的api
+        const res2 = await reqPay(pay);
+        // console.log(res2)
+        // 5查询订单
+        const res3 = await promise({
+          url:"/my/orders/chkOrder",
+          header:header,
+          data:{order_number},
+          method:"POST"
+        })
+        // console.log(res3, 'res3')
+        await Message({title:'支付成功'}) 
+        // 支付成功后跳转到订单页面
+        wx.navigateTo({
+          url: '/pages/order/index'
+        });
+          
+      }
+    } catch (err) {
+      await Message({title:'支付失败'})
     }
-      
   }
-})
+});
--- utils/wx-async.js
+++ utils/wx-async.js
@@ -2,69 +2,87 @@
  * 以Promise形式改造代码
  */
 
- /**
-  * 获取用户当前设置
-  */
- export const getSetting = () => {
-    return new Promise((resolve,reject)=>{
-        wx.getSetting({
-            success: (result) => {
-                resolve(result)
-            },
-            fail: (err) => {
-                reject(err)
-                console.log(err)
-            },
-        });     
-    })
- }
+/**
+ * 获取用户当前设置
+ */
+export const getSetting = () => {
+  return new Promise((resolve, reject) => {
+    wx.getSetting({
+      success: result => {
+        resolve(result);
+      },
+      fail: err => {
+        reject(err);
+        console.log(err);
+      }
+    });
+  });
+};
 
-  /**
-  * 修改用户当前设置
-  */
- export const openSetting = () => {
-    return new Promise((resolve,reject)=>{
-        wx.openSetting({
-            success: (result) => {
-                resolve(result)
-            },
-            fail: (err) => {
-                reject(err)
-            },
-        });     
-    })
- }
+/**
+ * 修改用户当前设置
+ */
+export const openSetting = () => {
+  return new Promise((resolve, reject) => {
+    wx.openSetting({
+      success: result => {
+        resolve(result);
+      },
+      fail: err => {
+        reject(err);
+      }
+    });
+  });
+};
 
-  /**
-  * 获取用户收货地址
-  */
- export const chooseAddress = () => {
-    return new Promise((resolve,reject)=>{
-        wx.chooseAddress({
-            success: (result) => {
-                resolve(result)
-            },
-            fail: (err) => {
-                reject(err)
-            },
-        });     
-    })
- }
+/**
+ * 获取用户收货地址
+ */
+export const chooseAddress = () => {
+  return new Promise((resolve, reject) => {
+    wx.chooseAddress({
+      success: result => {
+        resolve(result);
+      },
+      fail: err => {
+        reject(err);
+      }
+    });
+  });
+};
 
- /**
-  * 提示框信息
-  * {object} data
-  */
- export const Message = (data) => {
-    return new Promise(( resolve , reject ) =>{
-        wx.showToast( {
-            title: data.title,
-            success: (result) => {
-              resolve(result)
-            },
-            fail: (err) => {
-                reject(err)
-            },
-          });
-    })
- }
+/**
+ * 提示框信息
+ * {object} data
+ */
+export const Message = data => {
+  return new Promise((resolve, reject) => {
+    wx.showToast({
+      title: data.title,
+      success: result => {
+        resolve(result);
+      },
+      fail: err => {
+        reject(err);
+      }
+    });
+  });
+};
+
+/**
+ * 微信支付
+ * pay {object} 内置微信支付需要的参数的对象
+ */
+export const reqPay = pay => {
+  return new Promise((resolve, reject) => {
+    wx.requestPayment({
+      ...pay,
+      success: result => {
+        resolve(result);
+      },
+      fail: err => {
+        reject(err);
+      }
+    });
+  });
+};
