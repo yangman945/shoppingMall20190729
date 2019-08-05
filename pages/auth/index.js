ce164d5c8d0db70cadb0407dcb269e3c2677cd86
地主兄 <yangman945@163.com>
2019-08-05 08:41:00 +0800

    商品结算页面静态资源与购买商品计算完成

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,66 +1,27 @@
-// pages/auth/index.js
+import { promise } from "../../request/promise"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleUserInfo(e){
+    // console.log(e.detail)
+    // 获取生成token值的用户信息
+    const {encryptedData,rawData,iv,signature } = e.detail;
+    let code = '';
+    wx.login({
+      success: (result) => {
+        code = result.code
+        const data = {encryptedData,rawData,iv,signature,code:code}
+        promise({url:'/users/wxlogin',method:'post',data:data}).then(res=>{
+          const {token} = res.data.message
+          // 将获取到的token存入本地
+          wx.setStorageSync("token", token);
+          // 返回上一个页面
+          wx.navigateBack({
+            delta: 1
+          });
+        })
+      },
+    });
   }
 })
--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,57 @@
 // pages/pay/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    // 地址信息
+    address:{},
+    totalPrice:0,
+    totalNum:0,
+    cart:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(){
+    // 本地获取渲染于页面的信息
+    // 获取地址
+    let address = wx.getStorageSync("address") || {};
+    // 获取购物车
+    let cart = wx.getStorageSync("cart") || {};
+    // 遍历对象 将checked为false的属性删除
+    for (const key in cart) {
+        if(!(cart[key].checked)){
+          delete cart[key]
+        }  
+    }
+    let cartArr = Object.values(cart)
+    // 总价格
+    let totalPrice = 0
+    // 总数量
+    let totalNum = 0
+    // 将没有勾选的商品从购物车中过滤掉
+    cartArr.filter(v=>{
+       if(v.checked){
+        totalNum += v.num;
+        totalPrice += v.goods_price * v.num;
+      }
+    })
+    this.setData({
+      address,
+      totalPrice,
+      totalNum,
+      cart
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleOrderPay(){
+    // 对token进行获取判断 如果没有token进入授权页面获取
+    let token = wx.getStorageSync("token");
+    // console.log(token)
+    if(!token){
+      // 没有token去授权页面获取
+      wx.navigateTo({
+        url: '/pages/auth/index',
+      });
+        
+    }else{
+      // 有的话执行下一步
+      console.log('执行订单')
+    }
+      
   }
 })
